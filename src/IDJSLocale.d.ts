1a036be5863efc8e984150ecba7563b1933bf0b5
Humberto Bravo <humbandroid@gmail.com>
2024-11-30 17:47:16 -0300

    Tratando melhor a infraestrutura do projeto - XVI

---
+++ src/IDJSLocale.d.ts
@@ -0,0 +1,35 @@
+declare interface IDJSLocale {
+  name: string
+  language: string
+  weekdays?: string[]
+  months?: string[]
+  weekStart?: number
+  yearStart?: number
+  weekdaysShort?: string[]
+  monthsShort?: string[]
+  weekdaysMin?: string[]
+  ordinal?: (n: number) => number | string
+  formats: Partial<{
+    LT: string
+    LTS: string
+    L: string
+    LL: string
+    LLL: string
+    LLLL: string
+  }>
+  relativeTime: Partial<{
+    future: string
+    past: string
+    s: string
+    m: string
+    mm: string
+    h: string
+    hh: string
+    d: string
+    dd: string
+    M: string
+    MM: string
+    y: string
+    yy: string
+  }>
+}
