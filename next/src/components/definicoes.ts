ae348e34ab6aec5e4bc16c88d8a773f3cae3a951
Humberto Bravo <humbandroid@gmail.com>
2025-02-17 01:23:51 -0300

    Preparando ambiente para o port das funcionalidades construídas anteriormente na versão 0.0.4

---
+++ next/src/components/definicoes.ts
@@ -0,0 +1,9 @@
+import * as vscode from 'vscode';
+
+export const fdDefinicoes = {
+  // flawuldragon notes
+  comandoFlawuldragonNotes:
+    'flawuldragon.notes',
+  posicaoStatusBarItemFlawuldragonNotes:
+    vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 2500),
+};
--- next/src/components/flawuldragon-notes/flawuldragon_notes.ts
+++ next/src/components/flawuldragon-notes/flawuldragon_notes.ts
@@ -1,9 +1,10 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as vscode from "vscode";
+import { fdDefinicoes } from "../definicoes";
 
 export function flawuldragonNotes(context: vscode.ExtensionContext) {
-  vscode.commands.registerCommand("flawuldragon.notes", () => {
+  vscode.commands.registerCommand(fdDefinicoes.comandoFlawuldragonNotes, () => {
     let notes = vscode.window.createWebviewPanel("flawuldragon", "Flawuldragon Notes", vscode.ViewColumn.One, { enableForms: true, enableScripts: true });
     notes.title = 'Flawuldragon Notes';
     notes.iconPath = vscode.Uri.file(
@@ -17,4 +18,14 @@ export function flawuldragonNotes(context: vscode.ExtensionContext) {
     context.subscriptions.push(notes);
     return 0;
   });
+
+	// flawuldragon notes
+	let statusBarItem = fdDefinicoes.posicaoStatusBarItemFlawuldragonNotes;
+	statusBarItem.text = "$(flawuldragon-badge) Notes";
+	statusBarItem.show();
+	statusBarItem.tooltip = "The Flawuldragon's Notes";
+	statusBarItem.command = fdDefinicoes.comandoFlawuldragonNotes;
+	statusBarItem.backgroundColor = new vscode.ThemeColor("statusBarItem.warningBackground");
+	// statusBarItems
+	context.subscriptions.push(statusBarItem);
 }
